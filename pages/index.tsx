e94a4ac377c9e13980fc8f21fc76487bf3d328f4
jan kuwert <jan.kuwert@gmx.de>
2025-01-23 10:46:58 +0100

    fiox double attribute errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -117,7 +117,6 @@ export default function Home() {
               data-format="inline"
               data-version="5"
               data-options='{"settings":{"after_subscribe":{"action":"message","success_message":"Success! Now check your email to confirm your subscription.","redirect_url":""},"analytics":{"google":null,"fathom":null,"facebook":null,"segment":null,"pinterest":null,"sparkloop":null,"googletagmanager":null},"modal":{"trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"powered_by":{"show":false,"url":"https://kit.com/features/forms?utm_campaign=poweredby&amp;utm_content=form&amp;utm_medium=referral&amp;utm_source=dynamic"},"recaptcha":{"enabled":false},"return_visitor":{"action":"show","custom_content":""},"slide_in":{"display_in":"bottom_right","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"sticky_bar":{"display_in":"top","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15}},"version":"5"}'
-              minwidth="400 500 600 700 800"
             >
               <div data-style="clean">
                 <ul
@@ -133,9 +132,8 @@ export default function Home() {
                   <div className="flex gap-4 flex-col sm:flex-row w-full">
                     <div className="formkit-field w-full">
                       <input
-                        className="formkit-input"
+                        className="formkit-input p-2 bg-slate-100 w-full"
                         aria-label="First Name"
-                        className="p-2 bg-slate-100 w-full"
                         style={{
                           color: "rgb(0, 0, 0)",
                           borderColor: "rgb(227, 227, 227)",
@@ -149,9 +147,8 @@ export default function Home() {
                     </div>
                     <div className="formkit-field w-full">
                       <input
-                        className="formkit-input"
+                        className="formkit-input p-2 bg-slate-100 w-full"
                         name="email_address"
-                        className="p-2 bg-slate-100 w-full"
                         style={{
                           color: "rgb(0, 0, 0)",
                           borderColor: "rgb(227, 227, 227)",
@@ -167,8 +164,7 @@ export default function Home() {
                   </div>
                   <button
                     data-element="submit"
-                    className="formkit-submit"
-                    className="py-2 px-6"
+                    className="formkit-submit py-2 px-6"
                     style={{
                       color: "rgb(255, 255, 255)",
                       backgroundColor: "rgb(0, 0, 0)",
@@ -189,7 +185,9 @@ export default function Home() {
           </div>
         </dialog>
       </div>
-      <span className='text-slate-200 absolute bottom-3 right-3'>Photo: Xinrui Wan (CC-BY license)</span>
+      <span className="text-slate-200 absolute bottom-3 right-3">
+        Photo: Xinrui Wan (CC-BY license)
+      </span>
     </main>
   );
 }
